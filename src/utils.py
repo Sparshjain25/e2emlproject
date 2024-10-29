dbe327db9f570a9d5a861420b755c79e1d74bbe5
Sparshjain25 <sparshjain2502@gmail.com>
2024-10-29 00:27:07 -0400

    Hyper Parameter tuning done

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,9 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import classification_report,log_loss,f1_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+
 
 
 def save_object(file_path,obj):
@@ -21,13 +23,20 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(models)):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,para,cv=4)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
             # train_model_score=classification_report(y_train,y_train_pred)
